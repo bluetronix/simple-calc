b4fd79098bbf37ecb887a886f7899d97ab3ac3fb
bluetronix <145670912+bluetronix@users.noreply.github.com>
2024-04-19 21:56:34 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+document.getElementById("parag").style.display = "none";
+document.getElementById("para").style.display = "none";
+function change(){
+    document.getElementById("parag").style.display = "none";
+    document.getElementById("para").innerHTML = 'THERE IS A CHANGE ON THE SECIND LINE!'
+}
+function change1(){
+    document.getElementById("inpt").value = '1' ;
+}
+function change2(){
+    document.getElementById("inpt").value = '2' ;
+}
+function change3(){
+    document.getElementById("inpt").value = '3' ;
+}
+function change4(){
+    document.getElementById("inpt").value = '4' ;
+}
+function change5(){
+    document.getElementById("inpt").value = '5' ;
+}
+function change6(){
+    document.getElementById("inpt").value = '6' ;
+}
+function change7(){
+    document.getElementById("inpt").value = '7' ;
+}
+function change8(){
+    document.getElementById("inpt").value = '8' ;
+}
+function change9(){
+    document.getElementById("inpt").value = '9' ;
+}
+function change0(){
+    document.getElementById("inpt").value = '0' ;
+}
+function changea(){
+    document.getElementById("inpt").value = '+' ;
+}
+
+function changem(){
+    document.getElementById("inpt").value = '-' ;
+}
+function changep(){
+    document.getElementById("inpt").value = 'x' ;
+}
+function changed(){
+    document.getElementById("inpt").value = '/' ;
+}
